ead5db0312076f0e049e0f6e5d32186e35d3ec33
JVitor21 <jvlimasantos0@gmail.com>
2024-10-30 17:20:30 -0300

    Commit App.jsx

---
+++ meu-contador/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Contador from './contador'; // Importação do novo componente
+
+function App() {
+  return (
+    <div className="App">
+      <Contador /> {/* Adiciona o componente Contador aqui */}
+    </div>
+  );
+}
+
+export default App;
